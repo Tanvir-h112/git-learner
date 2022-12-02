ae36bc6532ec4702eaed0fe5c6f59726bf06ca76
Tanvir Hossain <rredoy100@gmail.com>
2022-12-02 12:10:39 +0600

    Added a new method called display to Hello.java

---
+++ Hello.java
@@ -0,0 +1,11 @@
+public class Hello {
+    public static void main (string[] args) {
+        system.out.println("Vs code is Awesome....");
+
+        greeting("tanvir Hossain")
+    }
+
+    public static void greeting(string name){
+        system.out.println('welcome ' + name);
+    }
+}
